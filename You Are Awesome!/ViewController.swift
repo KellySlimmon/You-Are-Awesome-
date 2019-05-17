f72e54431b4d384c2fa9401cd33e6ece0742cb2a
Kelly Slimmon <slimmon@bc.edu>
2019-05-17 15:26:43 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Kelly Slimmon on 5/17/19.
+//  Copyright © 2019 Kelly Slimmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
