dc25804e6faaf3b463794872a27f72c2a8b94e43
Kelly Slimmon <slimmon@bc.edu>
2019-06-03 21:58:40 -0500

    2.14 completed

---
+++ TypePlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myInt = 30000
+var myDouble = 3000.0
--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
 
 
-}
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Fantastic!",
+                        "You Are Great!",
+                        "You're The Bomb!",
+                        "U So Cooooool wow can i be you"]
+        var newIndex: Int
+        repeat{
+            newIndex = Int.random(in: 0..<messages.count)
+        } while index == newIndex
+                index = newIndex
+                messageLabel.text = messages[index]
+        }
+        
+        
+//        messageLabel.text = messages.randomElement()!
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1{
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+//
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
+//        messageLabel.textColor = UIColor.magenta
+    }
+    
+
 
