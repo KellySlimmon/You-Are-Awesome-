469f524dfcf2b3684ad6ccf89ecbe1e0b952fe5b
Kelly Slimmon <slimmon@bc.edu>
2019-06-07 12:32:07 -0500

    2.21 Adding Icon

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -73,27 +73,3 @@ class ViewController: UIViewController {
         }
     }
 }
-        
-//        messageLabel.text = messages.randomElement()!
-//        messageLabel.text = messages[index]
-//
-//        if index == messages.count - 1{
-//            index = 0
-//        } else {
-//            index = index + 1
-//        }
-//
-//        let message1 = "You Are Awesome!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        } else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        } else {
-//            messageLabel.text = message1
-//        }
-//        messageLabel.textColor = UIColor.magenta
-    
-
-
